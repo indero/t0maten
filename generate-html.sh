3e76d31411e2c1e14a5585d4ea374bb7bbfdc547
Manuel Hutter <manuel.hutter@vshn.ch>
2018-07-13 14:55:04 +0200

    Quote all variables
    This prevents issues with path names that contain whitespaces

--- generate-html.sh
+++ generate-html.sh
@@ -9,47 +9,47 @@ type pandoc >/dev/null 2>&1 || {
 
 cd "$(dirname "$0")"
 
-POMODORI_DIR=${POMODORI_DIR:-./pomodori}
-HTML_DIR=${HTML_DIR:-./html}
-CSS_DIR=${CSS_DIR:-./css}
+POMODORI_DIR="${POMODORI_DIR:-./pomodori}"
+HTML_DIR="${HTML_DIR:-./html}"
+CSS_DIR="${CSS_DIR:-./css}"
 
-cat > ${HTML_DIR}/index.md <<EOT
+cat > "${HTML_DIR}/index.md" <<EOT
 ---
 title: t0maten Overview
 ---
 EOT
 
 # Get the Mont and Year
-find ${POMODORI_DIR} -name '*.markdown' -exec basename -s .markdown "{}" \; \
+find "${POMODORI_DIR}" -name '*.markdown' -exec basename -s .markdown "{}" \; \
   | cut -d'-' -f1-2 \
   | sort -u \
   | while read -r month; do
 
   #Add the month to index.md
-  echo -e "\\n## $month\\n" >> ${HTML_DIR}/index.md
+  echo -e "\\n## ${month}\\n" >> "${HTML_DIR}/index.md"
 
   # For each day of a month create a Link
-  for dayofmonth in $(ls -r ${POMODORI_DIR}/${month}*); do
+  for dayofmonth in "${POMODORI_DIR}/${month}"*; do
 
     #echo $dayofmonth;
-    dayofmonth_basename=$(basename $dayofmonth .markdown)
+    dayofmonth_basename=$(basename "$dayofmonth" .markdown)
 
     pandoc \
       --self-contained \
       --metadata="title:Pomodoro ${day}" \
-      -o ${HTML_DIR}/${dayofmonth_basename}.html \
-      -c ${CSS_DIR}/pandoc.css \
-      -c ${CSS_DIR}/github2.css \
-      $dayofmonth
+      -o "${HTML_DIR}/${dayofmonth_basename}.html" \
+      -c "${CSS_DIR}/pandoc.css" \
+      -c "${CSS_DIR}/github2.css" \
+      "$dayofmonth"
 
-    echo "* [${dayofmonth_basename}](${dayofmonth_basename}.html)" >> ${HTML_DIR}/index.md
+    echo "* [${dayofmonth_basename}](${dayofmonth_basename}.html)" >> "${HTML_DIR}/index.md"
 
   done
 done
 
 pandoc \
   --self-contained \
-  -o ${HTML_DIR}/index.html \
-  -c ${CSS_DIR}/pandoc.css \
-  -c ${CSS_DIR}/github2.css \
-  ${HTML_DIR}/index.md
+  -o "${HTML_DIR}/index.html" \
+  -c "${CSS_DIR}/pandoc.css" \
+  -c "${CSS_DIR}/github2.css" \
+  "${HTML_DIR}/index.md"
